ec465b733b8092bf4452981e412931a2a4cb1d24
Dat_Pham <intern.pqdat@tma.com.vn>
2019-04-06 21:30:34 +0700

    revisione

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { OwlModule } from 'ngx-owl-carousel';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { NavComponent } from './nav/nav.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    NavComponent,
+    DashboardComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    OwlModule
   ],
   providers: [],
   bootstrap: [AppComponent]
