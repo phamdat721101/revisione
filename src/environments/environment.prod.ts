ca5484e1cb38f03bd2bcc7afad8687f70b196715
Dat_Pham <intern.pqdat@tma.com.vn>
2019-05-14 20:03:57 +0700

    deploy_web

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,5 +7,7 @@ export const environment = {
     projectId: "revisione-ea22e",
     storageBucket: "revisione-ea22e.appspot.com",
     messagingSenderId: "583878072593"
-  }
+  },
+  mapbox_private_token: 'sk.eyJ1Ijoic2FnZXdhbGwiLCJhIjoiY2oyYXdxemtoMDBibTJ3cXk3d3J5eG9wbiJ9.iLHhNhOOO_1X7gTaUuqX0Q',
+  mapbox_public_token: 'pk.eyJ1Ijoic2FnZXdhbGwiLCJhIjoiY2oyMmozNmxsMDBuZzJ3cWloaWZ3dWEzeiJ9.iH08SpUKtlsBC1ytys0OFQ'
 };
