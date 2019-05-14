2d20229a2f7c8fdfb2c9e02f7897c3cd14af2fd8
Dat_Pham <intern.pqdat@tma.com.vn>
2019-05-14 15:11:06 +0700

    upgrade_filter

--- src/app/explore/explore.component.ts
+++ src/app/explore/explore.component.ts
@@ -25,16 +25,20 @@ export class ExploreComponent implements OnInit {
   private geocoder: MapboxGeocoder;
   private geolocate: Mapboxgl.GeolocateControl;
   places: any;
-  placesFilter: any;
+  placesFilter: any[];
   destination: any;
   price: any;
+  arrPlaces: any[] = [];
+  arrPrices: any[] =[];
+  arrs: any[] = [];
+  arrs1: any[] =[];
   constructor(private router: Router,
     private placeService: FirebaseService) { 
     Mapboxgl.accessToken = environment.mapbox_public_token;
     this.style = 'mapbox://styles/sagewall/ciwf7ja64001o2psg2v73nsya';
     this.longitude = 107.0967741;
     this.latitude = 10.351835;
-    this.zoom = 10;
+    this.zoom = 10;    
   }
 
   ngOnInit() {
@@ -52,18 +56,44 @@ export class ExploreComponent implements OnInit {
     this.map.addControl(this.geocoder, 'top-left');
     this.geolocate = new Mapboxgl.GeolocateControl({});
     this.map.addControl(this.geolocate, 'top-right');
-    this.getPlaces();
-    this.getPlacesFilter();
+    this.getPlaces();          
   }
   getPlaces(){
-    this.placeService.getHotels().subscribe(ref => this.places = ref);
-  } 
-  getPlacesFilter(){
-    this.placeService.getHotels().subscribe(ref => this.placesFilter = ref);
+    this.placeService.getHotels().subscribe(ref => {
+      this.places = ref;       
+      for(var i = 0; i < ref.length; i++){
+        this.arrPlaces.push(this.places[i].payload.doc.data().place);
+        this.arrPrices.push(this.places[i].payload.doc.data().price)
+      }
+      this.arrs = this.filterItem(this.arrPlaces);
+      this.arrs1 = this.filterItem(this.arrPrices);
+      this.placesFilter = ref;
+    });
+  }   
+  filterItem(array){
+    return Array.from(new Set(array));
   }
   searchPlace(){
     this.placeService.searchPlaceFilter(this.destination,this.price).subscribe(result => {      
       this.placesFilter = result;
     });
+  }
+  getMap(place){
+    this.latitude = place.payload.doc.data().latitude;
+    this.longitude = place.payload.doc.data().longitude;
+    this.map = new Mapboxgl.Map({
+      container: this.mapCanvasNativeElement,
+      style: this.style,
+      center: [this.longitude, this.latitude],
+      zoom: this.zoom
+    });
+
+    this.geocoder = new MapboxGeocoder({
+      accessToken: Mapboxgl.accessToken,
+      mapboxgl: Mapboxgl
+    });
+    this.map.addControl(this.geocoder, 'top-left');
+    this.geolocate = new Mapboxgl.GeolocateControl({});
+    this.map.addControl(this.geolocate, 'top-right');
   }
 }
